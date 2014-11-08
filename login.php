f8ee04398235120bf620f616866c504320b93faa
onthered <jbuck594@gmail.com>
2014-11-08 14:29:54 -0500

    initial commit

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	</head>
+	<body>
+		<center>
+		<h1 class="title"> Login </h1>
+		<form action="" method = "post">
+			<p class="login">Username:
+				<input type="text" name="username"></p>
+			<p class="login">Password:
+				<input type="password" name="password"></p>
+			</p>
+		</form>
+		</center>
+	</body>
+</html>
