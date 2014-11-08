4f6a3dcea98fcbeae346cacbad84db5404cb14fd
onthered <jbuck594@gmail.com>
2014-11-08 16:19:13 -0500

    Login Update

--- checkAccount.php
+++ checkAccount.php
@@ -1,16 +1,34 @@
+<html>
+<h1><font size=5 color=red><b>Website.com</b></font><h1><hr><hr>
+<font size=3>
 <?php
-	include "./dbConnect.php";
-
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-
-	$result = mysql_query("SELECT * FROM users WHERE username ='$username' AND WHERE password = '$password'") or die(mysql_error());
-	$row = mysql_fetch_assoc($result);
-
-	if($row)
-	{
-		echo "successful";
-	}
-	else
-		echo "failed";
+session_start();
+$username = $_POST['username'];
+$password = $_POST['password'];
+if($username&&$password) {
+$connect = mysql_connect("localhost","root","") or die("Couldn't connect");
+mysql_select_db("phplogin") or die("couldn't find db");
+$query = mysql_query("SELECT * FROM users WHERE username='$username'");
+$numrows = mysql_num_rows($query);
+if ($numrows!=0)
+{
+while ($row = mysql_fetch_assoc($query))
+{
+$dbusername = $row['username'];
+$dbpassword = $row['password'];
+}
+if ($username==$dbusername&&$password==$dbpassword)
+{
+echo "login successful! <a href='member.php'>click here to access your page</a>";
+$_SESSION['username'] = $dbusername;
+}
+else
+echo "incorrect password!";
+}
+else
+die("That user doesn't exist!");
+}
+else
+die("Please enter a username and password");
 ?>
+</html>
