915f5c7679689dc16f96a32a2f7dd345c3248cf0
onthered <jbuck594@gmail.com>
2014-11-08 16:53:38 -0500

    Updating Registration

--- submitRegistration.php
+++ submitRegistration.php
@@ -9,7 +9,12 @@
 	{
 		$connect = mysql_connect("localhost","root","") or die("Couldn't connect");
 		mysql_select_db("phplogin") or die("couldn't find db");
-		mysql_query("INSERT INTO 'app_db', 'users' ('username', 'password', 'type', 'address') VALUES ('$username', '$password', '$type', '$address')";
+		$result = mysql_query("INSERT INTO 'users' ('username', 'password', 'type', 'address') VALUES ('$username', '$password', '$type', '$address')");
+
+		if($result)
+			echo "Successful registration";
+		else
+			echo "Failed registration";
 	}
 	else
 		die("Please enter the username, password, type, and address");
