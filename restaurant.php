4fc24d6476c22d01d6bc7cdfb1f7b08e14ea9ecc
Matthew Cieslak <sanibelmatt7@aol.com>
2014-11-08 20:26:14 -0500

    Updated restaurant and new php file

--- restaurant.php
+++ restaurant.php
@@ -1,3 +1,22 @@
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+ $("a").click(function(event){
+        event.preventDefault();
+        var uid = $(this).attr("id");
+         console.log(uid);
+         // post with item id to php file, update finished status in mysql
+         // if all rows with same order id have status flag 1 then order has
+         // been processed
+         $.post('UpdateStat.php', {id: uid}, function(data){
+              console.log(data);
+              if(data == "done"){
+                location.reload();
+              }
+            });
+        });
+      });
+</script>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
 
@@ -6,6 +25,7 @@
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+
 <nav class="navbar navbar-default" role="navigation">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -41,7 +61,7 @@
   $connect = mysql_connect("localhost","root","");
   mysql_select_db("app_db", $connect);
   // will change to select from orders where restaurant id = $rest_id ORDER BY(id) DESC
-  $query = mysql_query("SELECT id FROM orders WHERE rest_id='1' ");
+  $query = mysql_query("SELECT id FROM orders WHERE rest_id='1' AND flag = '0' ");
   $num_orders = mysql_num_rows($query);
   if($num_orders > 0){
     while($row = mysql_fetch_array($query)){
@@ -55,31 +75,42 @@
   foreach($all_orders as $order){
     $id = $order['id'];
     ?>
-    <center><tr align="center"><td><br>
+    <center>
+          <tr align="center"><td><br>
       <h3> <? echo "Order #".$id."\n"; ?></h3>
     </td></center><br>
       <?
     $get_items = mysql_query("SELECT * FROM ordered_items WHERE order_id = '$id'") or die (mysql_error());
       while($row_items = mysql_fetch_assoc($get_items)){
+          $ids[] = $row_items['id'];
           $item[] = $row_items['item'];
           $quantity[] = $row_items['quantity'];
           $order_id[] = $row_items['order_id'];
         }
         if($c == 0){
        foreach($item as $k => $v){
-         ?><tr class="success" align="center"><td><?
+         $itemid = $ids[$k];
+         ?><tr class="success" align="center" id="<? echo $itemid ?>">
+        <td>
+          <a style="width: 100%;" href="<? echo $itemid ?>" id="<? echo $itemid ?>">
+          <?
          echo $v." ".$quantity[$k];
-         ?></tr></td><?
+         ?></a></tr></td></a><?
       }
     }
      if($c > 0){
         array_splice($item,0,$oldsz);
         array_splice($quantity,0,$oldq);
         array_splice($order_id,0,$old_oi);
+        array_splice($ids,0,$old_ids);
         foreach($item as $key => $value){
-          ?><tr class="success" align="center"><td><?
+          $i_id = $ids[$key];
+          ?><tr class="success" align="center" id="<? echo $i_id ?>">
+           <td>
+              <a style="width: 100%;" href="<? echo $i_id ?>" id="<? echo $i_id ?>">
+            <?
           echo $value." ".$quantity[$key];
-          ?></tr></td><?
+          ?></tr></td></a><?
           }
   }
   ?>
@@ -89,6 +120,7 @@
  $oldsz = count($item);
  $oldq = count($quantity);
  $old_oi = count($order_id);
+ $old_ids = count($ids);
   }
  }
 
