a1c141da123a1f3fe4c79a0ad0ac72a74d58af0e
RichCerone <rcerone@live.com>
2014-11-08 21:57:34 -0500

    Rich  Updated sql

--- app_db.sql
+++ app_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.1.12
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2014 at 08:19 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Generation Time: Nov 09, 2014 at 03:56 AM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -31,9 +31,20 @@ CREATE TABLE IF NOT EXISTS `items` (
   `rest_id` int(11) NOT NULL,
   `price` decimal(10,0) NOT NULL,
   `name` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `rest_id` (`rest_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `rest_id`, `price`, `name`) VALUES
+(1, 1, '2', 'bagel with butter'),
+(2, 1, '3', 'bagel with cream cheese'),
+(3, 2, '2', 'dumplings'),
+(4, 2, '3', 'noodles'),
+(5, 3, '3', 'plain pizza'),
+(6, 3, '3', 'pizza with topping');
 
 -- --------------------------------------------------------
 
@@ -42,10 +53,11 @@ CREATE TABLE IF NOT EXISTS `items` (
 --
 
 CREATE TABLE IF NOT EXISTS `ordered_items` (
-  `id` int(11) NOT NULL COMMENT 'Item ID',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `order_id` int(11) NOT NULL,
-  `Quantity` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -73,7 +85,16 @@ CREATE TABLE IF NOT EXISTS `rests` (
   `name` varchar(30) NOT NULL,
   `address` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `rests`
+--
+
+INSERT INTO `rests` (`id`, `name`, `address`) VALUES
+(1, 'Rys Bagels', '100 Bagel Street'),
+(2, 'Hunan Wok', '105 Food Ave'),
+(3, 'Glassboro Pizzeria', '183 Pizza Street');
 
 -- --------------------------------------------------------
 
@@ -85,11 +106,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
-  `type` int(11) NOT NULL,
   `address` varchar(50) NOT NULL,
+  `type` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `address`, `type`) VALUES
+(1, 'test', 'test', 'test', 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
